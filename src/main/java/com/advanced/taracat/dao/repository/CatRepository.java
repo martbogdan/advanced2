142359fb021f2f701ee78b6ba3b7c15b2c1efb38
Alex Lvovych <admin@olmawritings.com>
2019-09-05 19:55:19 +0300

    Elements for Cat

--- src/main/java/com/advanced/taracat/dao/repository/CatRepository.java
+++ src/main/java/com/advanced/taracat/dao/repository/CatRepository.java
@@ -1,4 +1,8 @@
 package com.advanced.taracat.dao.repository;
 
-public interface CatRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CatRepository extends JpaRepository {
 }
--- src/main/java/com/advanced/taracat/service/CatService.java
+++ src/main/java/com/advanced/taracat/service/CatService.java
@@ -1,4 +1,9 @@
 package com.advanced.taracat.service;
 
+import com.advanced.taracat.dao.repository.CatRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CatService {
+    private CatRepository catRepository;
 }
