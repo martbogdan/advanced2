32635c164bc2fc18624239f1818ee6f19f33299c
martbogdan <mbogdan0123@gmail.com>
2019-09-12 16:27:02 +0300

    update Service, Repository, Entity (Tarakan)

--- src/main/java/com/advanced/taracat/dao/repository/TarakanRepository.java
+++ src/main/java/com/advanced/taracat/dao/repository/TarakanRepository.java
@@ -4,6 +4,9 @@ import com.advanced.taracat.dao.entity.Tarakan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TarakanRepository extends JpaRepository<Tarakan,Long> {
+    List<Tarakan> findAllByUser_Username (String username);
 }
--- src/main/java/com/advanced/taracat/service/TarakanService.java
+++ src/main/java/com/advanced/taracat/service/TarakanService.java
@@ -1,7 +1,39 @@
 package com.advanced.taracat.service;
 
+import com.advanced.taracat.dao.entity.Tarakan;
+import com.advanced.taracat.dao.repository.TarakanRepository;
+import com.advanced.taracat.exeptions.NotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TarakanService {
+   private TarakanRepository tarakanRepository;
+
+   public List<Tarakan> getAll(){
+       return tarakanRepository.findAll();
+   }
+
+    public List<Tarakan> getAllByUsername (String username){
+       return tarakanRepository.findAllByUser_Username(username);
+    }
+
+    public Tarakan getTarakanById (Long id){
+       return tarakanRepository.findById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Tarakan create (Tarakan tarakan){
+       return tarakanRepository.save(tarakan);
+    }
+
+    public void delete (Long id){
+        Optional<Tarakan> toDelete = tarakanRepository.findById(id);
+        if (toDelete.isPresent()){
+            tarakanRepository.delete(toDelete.get());
+        }
+    }
+
+
 }
