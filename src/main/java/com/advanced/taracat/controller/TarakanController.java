550ec2de5417d55cfc17b62a575d8e6f24421d7c
martbogdan <mbogdan0123@gmail.com>
2019-09-19 12:41:14 +0300

    updated TarakanController

--- src/main/java/com/advanced/taracat/controller/TarakanController.java
+++ src/main/java/com/advanced/taracat/controller/TarakanController.java
@@ -11,7 +11,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class TarakanController {
@@ -24,25 +28,45 @@ public class TarakanController {
 
     @GetMapping("/tarlist")
     public String listTarakan (Model model) {
+        String tarError = "";
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("tarakans", tarakanRepository.findAllByUser_Username(authentication.getName()));
+        model.addAttribute("tar_error", tarError);
         return "tarlist";
     }
 
-    /*@PostMapping("/addtar")
-    public String addTarakan (@RequestParam String tarname){
+    @PostMapping("/addtar")
+    public String addTarakan (@RequestParam String tarname, Model model){
         Tarakan tarakan = new Tarakan();
+        Tarakan tarakanDB;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userName = authentication.getName();
         User currentUser = userService.getUserByUsername(userName);
-        tarakan.setUser(currentUser);
-        tarakan.setTarname(tarname);
-        tarakan.setLevel(0);
-        tarakan.setExperience(0);
-        tarakan.setStep(3);
-        tarakanRepository.save(tarakan);
+
+        String tarError;
+
+        List<Tarakan> tarCount = tarakanRepository.findAllByUser_Username(userName);
+        int tarCountSize = tarCount.size();
+        if (tarCountSize >= 5){
+            tarError = "У вас вже існує 5 тараканів, видаліть одного або декілька та добавте нового";
+        } else {
+            tarakanDB = tarakanRepository.findByTarname(tarname);
+            if(tarakanDB != null){
+                tarError = "Таракан з таким іменем вже існує";
+            } else {
+                tarakan.setUser(currentUser);
+                tarakan.setTarname(tarname);
+                tarakan.setLevel(1);
+                tarakan.setExperience(0);
+                tarakan.setStep(3);
+                tarakanRepository.save(tarakan);
+                tarError = "";
+            }
+        }
+        model.addAttribute("tarakans", tarakanRepository.findAllByUser_Username(authentication.getName()));
+        model.addAttribute("tar_error", tarError);
         return "tarlist";
-    }*/
+    }
 
     @GetMapping("/delete_tarakan")
     public String deleteTarakan (@RequestParam Long tarId){
@@ -50,6 +74,6 @@ public class TarakanController {
         if (deletedTarakan != null){
             tarakanRepository.delete(deletedTarakan);
         }
-         return "tarlist";
+         return "forward:/tarlist";
     }
 }
