9a4e955d8ae07bc919068e0e39849e5442c93ff4
martbogdan <mbogdam0123@gmail.com>
2019-10-07 20:22:29 +0300

    runJ

--- src/main/java/com/advanced/taracat/dao/repository/RunJRepository.java
+++ src/main/java/com/advanced/taracat/dao/repository/RunJRepository.java
@@ -4,6 +4,10 @@ import com.advanced.taracat.dao.entity.RunJ;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RunJRepository extends JpaRepository<RunJ, Long> {
+    List<RunJ> findAllByUser_Id(Long id);
+    List<RunJ> findAllByUser_Username(String username);
 }
--- src/main/java/com/advanced/taracat/service/RunJService.java
+++ src/main/java/com/advanced/taracat/service/RunJService.java
@@ -1,8 +1,36 @@
 package com.advanced.taracat.service;
 
+import com.advanced.taracat.dao.entity.RunJ;
+import com.advanced.taracat.dao.entity.User;
+import com.advanced.taracat.dao.repository.RunJRepository;
+import com.advanced.taracat.exeptions.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RunJService {
+    @Autowired
+    RunJRepository runJRepository;
+public List<RunJ> getAll(){
+    return runJRepository.findAll();
+}
+public List<RunJ> getAllByUserId (Long id){
+    return runJRepository.findAllByUser_Id(id);
+}
+public List<RunJ> getAllByUserName (String username){
+    return runJRepository.findAllByUser_Username(username);
+}
+public RunJ getRunJById (Long id){
+    return runJRepository.findById(id).orElseThrow(NotFoundException::new);
+}
+public RunJ create (User user){
+    RunJ runJ = new RunJ();
+    runJ.setUser(user);
+    runJ.setQuantityRunning(0);
+    runJ.setBotConfiguration("");
+    return runJ;
+}
 
 }
