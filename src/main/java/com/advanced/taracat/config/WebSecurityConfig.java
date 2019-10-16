33e315418bd582561225c24cdc020404ae0dd6d5
martbogdan <mbogdan0123@gmail.com>
2019-10-17 02:17:55 +0300

    image for main page

--- src/main/java/com/advanced/taracat/config/WebSecurityConfig.java
+++ src/main/java/com/advanced/taracat/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -38,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/login","/registration").permitAll()
+                    .antMatchers("/","/login","/registration").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
@@ -59,5 +60,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery(usersQuery)
                 .authoritiesByUsernameQuery(rolesQuery);
 
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+//    necessary to prevent security from being applied to the resources
+//    such as CSS, images and javascripts
+        web
+                .ignoring()
+                .antMatchers(
+                        "/resources/**",
+                        "/static/**",
+                        "/templates/**",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**",
+                        "/style_font/**",
+                        "/style_font/js/**"
+                );
     }
 }
