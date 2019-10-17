a8f40fe28a9ca77d37590d88ededb1b617d0d640
martbogdan <mbogdam0123@gmail.com>
2019-10-17 18:08:30 +0300

    cat

--- src/main/java/com/advanced/taracat/config/WebSecurityConfig.java
+++ src/main/java/com/advanced/taracat/config/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/login","/registration").permitAll()
+                    .antMatchers("/","/login","/registration").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
